d66e9dba486e9ef7ce4b27449aa19181a6aeb21b
uobaku <150378945+uobaku@users.noreply.github.com>
2023-11-10 12:17:53 +0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.getElementById("homebtn").addEventListener("click", function() {
+    location.href = "index.html"
+});
+
+document.getElementById("insta").addEventListener("click", function() {
+    location.href = "https://www.instagram.com/ushaqlarimizaoyredek/"
+});
+
+document.getElementById("tiktok").addEventListener("click", function() {
+    location.href = "https://www.tiktok.com/@ushaqlarimizaoyredek?_t=8h5APnACVo9&_r=1"
+});
+
+document.getElementById("linkedin").addEventListener("click", function() {
+    location.href = "https://www.linkedin.com/in/ushaqlarimiza-oyredek-2b9051296/"
+});
+
+document.getElementById("prtbtn").addEventListener("click", function() {
+    location.href = "partners.html"
+});
+
+
+document.getElementById("facebook").addEventListener("click", function() {
+    location.href = "https://www.facebook.com/profile.php?id=61552572833316"
+});
