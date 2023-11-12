b02e13c5c718510bccd0032158c1a241333290a2
uobaku <150378945+uobaku@users.noreply.github.com>
2023-11-12 14:07:47 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,37 @@ document.getElementById("prtbtn").addEventListener("click", function() {
     location.href = "partners.html"
 });
 
+document.getElementById("hstevntbtn").addEventListener("click", function() {
+    location.href = "hosted.html"
+});
+
+document.getElementById("ourstorybtn").addEventListener("click", function() {
+    location.href = "story.html"
+});
+document.getElementById("ourteambtn").addEventListener("click", function() {
+    location.href = "team.html"
+});
 
 document.getElementById("facebook").addEventListener("click", function() {
     location.href = "https://www.facebook.com/profile.php?id=61552572833316"
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menuBtn");
+    const dropdowns = document.querySelectorAll('.dropdownmob');
+    const header = document.getElementById('headerBar')
+    const titlediv = document.getElementById('titlediv')
+    const headerimg = document.getElementById('headerimg')
+
+    menuBtn.addEventListener("click", function () {
+        menuBtn.classList.toggle('open');
+        header.classList.toggle('open');
+        titlediv.classList.toggle('open');
+        if (headerimg != null){
+            headerimg.classList.toggle('open');
+        }
+        dropdowns.forEach(function (dropdown) {
+            dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+        });
+    });
 });
